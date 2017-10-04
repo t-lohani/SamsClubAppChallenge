f516e4ac6c78b886d4b17cce775722b9bdff5716
Tarun Lohani <t.lohani@gmail.com>
2017-10-03 23:54:25 -0400

    Made splash screen

---
+++ SamsClubApp/app/src/main/java/com/samsclub/app/samsclubapp/activities/HomeScreen.java
@@ -0,0 +1,15 @@
+package com.samsclub.app.samsclubapp.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.samsclub.app.samsclubapp.R;
+
+public class HomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
